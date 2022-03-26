571e5013f0f1c0046f43e5e8e033959589a38f67
bringajmit <bringajmit@gmail.com>
2022-03-26 08:51:22 -0300

    Correcion js

--- js/acciones.js
+++ js/acciones.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //var valor1=0;
 //let valor2=0;
 //let total=0;
@@ -19,28 +18,3 @@ const ConcatenarNombre=()=>{
     var apellido=document.getElementById("apellido").value;
     mostrar=`Hola ${nombre} ${apellido}`;
     document.getElementById("visor").value=mostrar;
-
-=======
-//var valor1=0;
-//let valor2=0;
-//let total=0;
-
-const sumar = () => {
-  //  valor1=parseFloat(document.getElementById("valor1").value);
-    //valor2=parseFloat(document.getElementById("valor2").value);
-    var valor1=parseFloat(document.getElementById("valor1").value);
-    var valor2=parseFloat(document.getElementById("valor2").value);
-    total=valor1+valor2;
-    console.log(total);
-    document.getElementById("resultado").value=parseFloat(total);
-}
-
-const ConcatenarNombre=()=>{
-    var mostrar="";
-    var nombre=document.getElementById("nombre").value;
-    var apellido=document.getElementById("apellido").value;
-    mostrar=`Hola ${nombre} ${apellido}`;
-    document.getElementById("visor").value=mostrar;
-
->>>>>>> 6a186d9 (Ejercicio de Practica)
-}
